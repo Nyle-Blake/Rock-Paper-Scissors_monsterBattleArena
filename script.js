5beab8672ab47a5bd5bccc295d3a4cfc017f90de
Nyle-Blake <Nyleblake1@gmail.com>
2024-01-16 18:29:22 +0000

    add while loop to loop over the game and show final result of the game

--- script.js
+++ script.js
@@ -4,24 +4,51 @@
 // 4. return result
 // 5. (Make the game loop 5 times)
 
-const getChoices = () => {
-  const computerChoice = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
-  const userChoice = prompt("choose one: Rock, Paper, or Scissors")
+const rockPaperScissors = () => {
+  let computerChoice;
+  let userChoice;
   let result = "";
+  let count = 0;
+  let userScore = 0;
+  let computerScore = 0;
+  let finalResult = "";
 
-  if (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors") {
-    result = "Incorrect input, Please enter Rock, Paper, or Scissors"
-  } else if (computerChoice == "Rock" && userChoice == "Rock" || computerChoice == "Paper" && userChoice == "Paper" || computerChoice == "Scissors" && userChoice == "Scissors") {
-    result = "Draw"
-  } else if (userChoice == "Rock" && computerChoice == "Scissors" || userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissors" && computerChoice == "Paper") {
-    result = "You won this round!"
+  // while count <= 5 play game  
+
+  while(count <= 5) {
+
+    computerChoice = ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
+    userChoice = prompt("choose one: Rock, Paper, or Scissors")
+
+    if (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors") {
+      result = "Incorrect input, Please enter Rock, Paper, or Scissors"
+    } else if (computerChoice == "Rock" && userChoice == "Rock" || computerChoice == "Paper" && userChoice == "Paper" || computerChoice == "Scissors" && userChoice == "Scissors") {
+      result = "This round is a draw"
+    } else if (userChoice == "Rock" && computerChoice == "Scissors" || userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Scissors" && computerChoice == "Paper") {
+      result = "You won this round!"
+      userScore++
+    } else {
+      result = "You lost this round!"
+      computerScore++
+    }
+
+    count++
+    console.log(`You chose ${userChoice} and the computer chose ${computerChoice} so ${result}`)
+  }
+    
+  if (userScore == computerScore) {
+    finalResult = "The game was a draw!"
+  } else if (userScore > computerScore) {
+    finalResult = "You won the game!"
   } else {
-    result = "You lost this round!"
+    finalResult = "You lost the game!"
   }
 
-  return `The computer chose ${computerChoice} so ` + result
+  return `Your score was ${userScore} and the computer's score was ${computerScore}, so ${finalResult}`
+
 }
 
+rockPaperScissors()
 // rock => scissors
 // scissors => paper
 // paper => rock
